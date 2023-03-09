68fa809e0c0ec7e5a096b536933ddcc8f0c5ea1a
Lincoln Wasden <lincolndwasden@gmail.com>
2023-03-09 10:35:33 -0600

    added dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-const Dashboard = ({ user }) => {
-return (
-<div>
-<h1>Welcome</h1>
-{/* You can add more components or functionality here for your dashboard */}
-</div>
-);
+const Dashboard = () => {
+  const [iframeKey, setIframeKey] = useState(0);
+
+  const handleResize = () => {
+    setIframeKey((prevKey) => prevKey + 1);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <iframe
+        src="https://app.gogrow.com/dashboard/share/d338bc61713836154e82fcf0d3948477"
+        style={{ width: "100%", height: "100%", border: "none" }}
+        title="Grow Dashboard"
+        allowFullScreen
+        key={iframeKey}
+      />
+    </div>
+  );
 };
 
 export default Dashboard;
